347724512512da559074500f08a539f0fd172404
Yatik Srivastava <yatiksrivastava1@gmail.com>
2023-12-08 22:59:18 +0530

    ::counter

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
+import "./index.css" 
 
 const App = () => {
+
+  const [value, setValue] = useState(0)
+
+  const addClick = () => {
+       setValue(value+1)
+  }
+
+  const reduceClick = () => [
+     setValue(value-1)
+  ]
+
   return (
-    <div>
-      App
-    </div>
+   <div className='App'>
+        <h1>Counter App</h1>
+        <h3>Counter value : {value} 🔥🔥 </h3>
+
+        <div className="buttons">
+         <button onClick={addClick}>Add value</button> {" "}
+         <button onClick={reduceClick}>Reduce value</button>
+        </div>
+   </div>
   )
 }
 
